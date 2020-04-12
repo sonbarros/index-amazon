85cdc0f395ed78fb25bbb82d0f2f975626300a44
Anderson Sousa <anderson21br@gmail.com>
2020-04-11 22:02:20 -0300

    Teste de implantacao do laravel em pasta anterior

--- index.php
+++ index.php
@@ -23,4 +23,6 @@ if($_SERVER["SERVER_NAME"] == "www.luarcoworking.com.br") {
 	header("location: /empflowp/wp-admin/");
 }else if($_SERVER["SERVER_NAME"] == "http://ec2-18-220-210-235.us-east-2.compute.amazonaws.com") {
 	header("location: ../supermercado/public");
+}else {
+	echo $_SERVER["SERVER_NAME"];
 }
